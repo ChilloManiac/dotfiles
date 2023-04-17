3a6e6d6deb1c74a9e231cd5c4060ad930c601f17
Christoffer Norbjerg <Christoffer.noerbjerg@consultant.lego.com>
2023-04-17 21:03:02 +0200

    done

--- confs/.config/nvim/lua/keybinds.lua
+++
@@ -1,6 +0,0 @@
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
