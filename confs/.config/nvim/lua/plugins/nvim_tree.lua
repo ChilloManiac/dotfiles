64638b941e390e8ed3e6c36484078a46a3d27cb1
Christoffer Norbjerg <Christoffer.noerbjerg@consultant.lego.com>
2023-04-20 20:56:32 +0200

    Try nvim tree

---
+++ confs/.config/nvim/lua/plugins/nvim_tree.lua
@@ -0,0 +1,28 @@
+local M = {
+	"kyazdani42/nvim-tree.lua",
+	cmd = "NvimTreeToggle",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	keys = {
+		{ "<leader>n", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+	},
+}
+
+M.config = function()
+	-- OR setup with some options
+	require("nvim-tree").setup({
+		sort_by = "case_sensitive",
+		view = {
+			adaptive_size = true,
+		},
+		renderer = {
+			group_empty = true,
+		},
+		update_focused_file = {
+			enable = true,
+			update_cwd = true,
+			ignore_list = {},
+		},
+	})
+end
+
+return M
